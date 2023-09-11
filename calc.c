b41b1717205feadadb240463bd08da7df788ef16
DevFull-Infinity <126342816+DevFull-Infinity@users.noreply.github.com>
2023-09-11 18:07:23 -0300

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,78 +1,50 @@
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
 
-int main(void){
+int main(){
     setlocale(LC_ALL,"portuguese");
     float num1,num2,result;
     int cond;
-    char op[2];
+    char op;
 
-    printf("Escolha uma operaÁ„o:\n+ - SOMA\n- -> SUBTRA«√O\n* -> MULTIPLICA«√O\n/ - DIVIS√O\n");
+    printf("Escolha uma opera√ß√£o:\n+ - SOMA\n- -> SUBTRA√á√ÉO\n* -> MULTIPLICA√á√ÉO\n/ - DIVIS√ÉO\n");
 
     while(cond != 1){
         
-        printf("OperaÁ„o:");
-        gets(op);
+        printf("Opera√ß√£o:");
+        scanf(" %c",&op);
 
-        if(strcmp(op,"+") == 0){
-            printf("Digite um numero:");
-            scanf("%f",&num1);
+        printf("\nDigite um numero:");
+        scanf(" %f",&num1);
 
-            printf("Digite outro numero:");
-            scanf("%f",&num2);
+        printf("Digite outro numero:");
+        scanf(" %f",&num2);
 
-            result = num1+num2;
-            printf("Resultado: %f\n",result);
 
-            printf("\nDeseja Sair?\n1 - S\n2 - N\n");
-            scanf("%d",&cond);
+        switch(op){
+            case '+':
+                result = num1+num2;
+                printf("Resultado: %f\n",result);
+                break;
+
+            case '-':
+                result = num1-num2;
+                printf("Resultado: %f\n",result);
+                break;
+
+            case '*':
+                result = num1*num2;
+                printf("Resultado: %f\n",result);
+                break;
+            
+            case '/':
+                result = num1/num2;
+                printf("Resultado: %f\n",result);
+                break;
         }
             
-        if(strcmp(op,"-") == 0){
-            printf("Digite um numero:");
-            scanf("%f",&num1);
-
-            printf("Digite outro numero:");
-            scanf("%f",&num2);
-
-            result = num1-num2;
-
-            printf("Resultado: %f\n",result);
-
-            printf("\nDeseja Sair?\n1 - S\n2 - N\n");
-            scanf("%d",&cond);
-        }
-
-        if(strcmp(op,"*") == 0){
-            printf("Digite um numero:");
-            scanf("%f",&num1);
-
-            printf("Digite outro numero:");
-            scanf("%f",&num2);
-
-            result = num1*num2;
-
-            printf("Resultado: %f\n",result);
-
-            printf("\nDeseja Sair?\n1 - S\n2 - N\n");
-            scanf("%d",&cond);
-        }
-
-        if(strcmp(op,"/") == 0){
-            printf("Digite um numero:");
-            scanf("%f",&num1);
-
-            printf("Digite outro numero:");
-            scanf("%f",&num2);
-
-            result = num1/num2;
-
-            printf("Resultado: %f\n",result);
-
-            printf("\nDeseja Sair?\n1 - S\n2 - N\n");
-            scanf("%d",&cond);
-        }
+        printf("\nDeseja Sair?\n1 - S\n2 - N\n");
+        scanf(" %d",&cond);
     } 
     return 0; 
 }
